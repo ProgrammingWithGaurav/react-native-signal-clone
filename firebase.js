82ffb1a90f07c2473a75b1308fb6accf04bb9c87
ProgrammingWithGaurav <gaurav2499kumar@gmail.com>
2022-01-09 10:15:04 +0530

    Added Login Screen

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjWcWPbKsXCs93mww6B7VWid4RkiRPYxA",
+    authDomain: "react-native-projects-80c2f.firebaseapp.com",
+    projectId: "react-native-projects-80c2f",
+    storageBucket: "react-native-projects-80c2f.appspot.com",
+    messagingSenderId: "698804821287",
+    appId: "1:698804821287:web:4bb34e66cdc7a43fc69d02",
+    measurementId: "G-QSPTY5CN8T"
+  };
